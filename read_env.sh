b7320a5862c23b3573aac2ef80d3a1042a77647a
thejaswitricon <thejaswi.cy@triconinfotech.com>
2023-05-25 17:54:54 +0530

    feat:test

--- read_env.sh
+++ read_env.sh
@@ -2,8 +2,12 @@
 
 __SECRET="$1"
 
-# Remove the prefix "__" from the key
-key="${__SECRET:2}"
+# Check if the secret name starts with "__" and remove it if true
+if [[ $__SECRET == __* ]]; then
+  key="${__SECRET:2}"
+else
+  key="$__SECRET"
+fi
 
 # Generate a JSON-encoded map of string keys and string values
 json_output="{ \"$key\": \"$__SECRET\" }"
