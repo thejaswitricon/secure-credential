a90c654d7df7d666f4a7ccc1377e3f5ff97d1791
thejaswitricon <thejaswi.cy@triconinfotech.com>
2023-05-25 17:43:23 +0530

    feat:test

--- read_env.sh
+++ read_env.sh
@@ -1,24 +1,8 @@
 #!/bin/bash
 
-# Start with an empty JSON object
-echo "{"
+__SECRET="$1"
 
-# Loop through each secret and append it to the JSON object
-first=true
-for secret_name in $(env | grep -oP "^__\K.*"); do
-  value=$(printenv "$secret_name")  # Fetch the secret value dynamically using the secret name
+echo "The __SECRET value is: $__SECRET"
 
-  # Remove the "__" prefix from the secret name
-  secret_name=${secret_name/#__/}
 
-  # Append the secret to the JSON object
-  if [ "$first" = true ]; then
-    first=false
-  else
-    echo ","
-  fi
-  echo -n "\"$secret_name\": \"$value\""
-done
-
-# Close the JSON object
-echo "}"
+# Rest of your script
