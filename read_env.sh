01f2eca3931669804359cf2456133c00af513f5f
thejaswitricon <thejaswi.cy@triconinfotech.com>
2023-05-26 10:40:35 +0530

    feat:test

--- read_env.sh
+++ read_env.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-secrets_file=$1
+__SECRET="$1"
 
-# Read the secrets file and store the contents in a variable
-secrets_json=$(cat "$secrets_file")
+# Create an associative array with the key-value pair
+declare -A output
+output["__SECRET"]=$__SECRET
 
-# Access the secrets individually (example)
-__SECRET1=$(jq -r '.__SECRET1' <<< "$secrets_json")
-__SECRET2=$(jq -r '.__SECRET2' <<< "$secrets_json")
+# Convert the associative array to JSON using jq
+json_output=$(declare -p output | jq -n 'from_entries')
 
-# Use the secrets as needed
-echo "The value of __SECRET1 is: $__SECRET1"
-echo "The value of __SECRET2 is: $__SECRET2"
+echo "$json_output"
