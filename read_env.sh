9d5071490d6623e0dded4aed9ee53208e719195e
thejaswitricon <thejaswi.cy@triconinfotech.com>
2023-05-26 10:22:04 +0530

    feat:test

--- read_env.sh
+++ read_env.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
 secrets_file=$1
+secrets_map={}
 
-# Read the secrets file and export the variables
-export $(cat "$secrets_file")
+# Read the secrets file and populate the secrets map
+while IFS='=' read -r key value; do
+  secrets_map[$key]=$value
+done < "$secrets_file"
+
+# Encode the secrets map as JSON
+secrets_json=$(jq -n --argjson secrets "${secrets_map[@]}" '$secrets')
 
 # Access the secrets individually (example)
-echo "The value of __SECRET1 is: $__SECRET1"
-echo "The value of __SECRET2 is: $__SECRET2"
+echo "The value of __SECRET1 is: ${secrets_map["__SECRET1"]}"
+echo "The value of __SECRET2 is: ${secrets_map["__SECRET2"]}"
+
+# Output the JSON-encoded secrets map
+echo "$secrets_json"
