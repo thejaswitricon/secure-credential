42e5ad3b9a9f001f0270912be30ed023523bfee1
thejaswitricon <thejaswi.cy@triconinfotech.com>
2023-05-26 10:19:59 +0530

    feat:test

--- read_env.sh
+++ read_env.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-# Parse input as JSON and iterate over key-value pairs
-jq -r 'to_entries[] | select(.key | startswith("__")) | .key + "=" + .value' |
-while IFS='=' read -r key value; do
-  # Remove the leading "__" from the key
-  secret_key="${key:2}"
+secrets_file=$1
 
-  # Process the secret value as per your requirement
-  # For example, you can export it as an environment variable
-  export "$secret_key"="$value"
+# Read the secrets file and export the variables
+export $(cat "$secrets_file")
 
-  # Print the exported key-value pair (optional)
-  echo "Exported: $secret_key=$value"
-done
+# Access the secrets individually (example)
+echo "The value of __SECRET1 is: $__SECRET1"
+echo "The value of __SECRET2 is: $__SECRET2"
