19064219d8abcade7dbca8931200e4b31c53c539
thejaswitricon <thejaswi.cy@triconinfotech.com>
2023-05-26 11:14:56 +0530

    feat:test

--- read_env.sh
+++ read_env.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# Read the "__SECRET" secret from the environment
-SECRET=$1
+# Extract the secret value
+secret_value=${__SECRET:-}
 
-# Remove the "__" prefix from the secret key
-SECRET_KEY="${SECRET//__}"
+# Create a JSON-encoded map
+secrets_map="{ \"__SECRET\": \"$secret_value\" }"
 
-# Create a JSON object with the modified secret key and value
-JSON="{ \"$SECRET_KEY\": \"$SECRET\" }"
-
-# Store the JSON-encoded secret in a file
-echo $JSON > secret.json
+# Output the JSON-encoded map
+echo "$secrets_map"
