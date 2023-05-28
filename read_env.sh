2ba812f43987710437ea469088d227a579d31061
thejaswitricon <thejaswi.cy@triconinfotech.com>
2023-05-28 10:57:58 +0530

    fix:test

--- read_env.sh
+++ read_env.sh
@@ -3,8 +3,14 @@
 # Read the secret from the workflow input
 secret="$1"
 
-# Create a JSON-encoded map
-json="{ \"secret\": \"$secret\" }"
+# Create an empty associative array
+declare -A json_map
+
+# Set the "secret" key to the provided secret value
+json_map["secret"]="$secret"
+
+# Convert the associative array to JSON
+json=$(jq -n "$(declare -p json_map)")
 
 # Output the JSON-encoded map
 echo "$json" > secret.json
