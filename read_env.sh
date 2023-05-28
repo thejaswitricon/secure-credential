b8a0af40a056612685e41e5c0dd148312e1087a4
thejaswitricon <thejaswi.cy@triconinfotech.com>
2023-05-28 20:53:35 +0530

    fix:test

--- read_env.sh
+++ read_env.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# Store the secret key and value
-SECRET_KEY="$1"
-SECRET_VALUE="$2"
-
 # Create a JSON object with the secret key-value pair
-JSON_OBJECT="{ \"$SECRET_KEY\": \"$SECRET_VALUE\" }"
+JSON_OBJECT="{ \"$1\": \"$2\" }"
 
 # Output the JSON object
 echo "$JSON_OBJECT"
