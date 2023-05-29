7a3cd9677256000b60c39ef8f5f2b6fb2ee5060f
thejaswitricon <thejaswi.cy@triconinfotech.com>
2023-05-29 15:01:59 +0530

    fix:test

--- read_env.sh
+++ read_env.sh
@@ -1,27 +1,8 @@
 #!/bin/bash
 
-# Fetch the secrets from GitHub Secrets
-API_URL="https://api.github.com/repos/thejaswitricon/secure-credential/actions/secrets"
-REPO_OWNER="thejaswitricon"
-REPO_NAME="secure-credential"
-GITHUB_TOKEN="GITHUB_SECRET"
+# Read the secrets from the JSON file
+secrets=$(cat secrets.json)
 
-secret_value=$(curl -s -H "Authorization: Bearer $GITHUB_TOKEN" \
-  "$API_URL/repos/thejaswitricon/secure-credential/actions/secrets/__SECRET" | \
-  jq -r '.secret.value')
-
-# Output the secrets as JSON
-echo '{ "my_secret": "'"$secret_value"'"}'
-
-
-
-
-
-
-# #!/bin/bash
-
-# # Create a JSON object with the secret key-value pair
-# JSON_OBJECT="{ \"$1\": \"$2\" }"
-
-# # Output the JSON object
-# echo "$JSON_OBJECT"
+# Use the secrets as needed
+echo "Secrets:"
+echo "$secrets"
