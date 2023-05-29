ec94505c3c4cf50063de45ccf0ee674520f45ed4
thejaswitricon <thejaswi.cy@triconinfotech.com>
2023-05-29 15:12:37 +0530

    fix:test

--- read_env.sh
+++ read_env.sh
@@ -3,5 +3,6 @@
 # Read the secrets from the JSON file
 secrets=$(cat secrets.json)
 
-# Convert the inputs to JSON-encoded map and output directly
-echo "$secrets" | jq -n 'split("\n") | map(split("=")) | map({ (.[0]): .[1] }) | add'
+# Use the secrets as needed
+echo "Secrets:"
+echo "$secrets"
