3d43a7304015eaceeaea544c8d19e24d8f8f90c3
thejaswitricon <thejaswi.cy@triconinfotech.com>
2023-05-29 15:20:03 +0530

    fix:test

--- read_env.sh
+++ read_env.sh
@@ -6,6 +6,5 @@ secrets=$(cat secrets.json)
 # Extract the secret keys starting with "__" and output as JSON map
 extracted_secrets=$(echo "$secrets" | jq 'with_entries(select(.key | startswith("__")))')
 
-# Output the extracted secrets
-echo "Extracted Secrets:"
+# Output the extracted secrets as JSON
 echo "$extracted_secrets"
