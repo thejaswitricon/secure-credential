8595dc8f910dc9ea4e23621476208c2609b8a19d
thejaswitricon <thejaswi.cy@triconinfotech.com>
2023-05-31 11:11:28 +0530

    fix:test

---
+++ read_env.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Read the secrets from the JSON file
+secrets=$(cat secrets.json)
+
+# Extract the secret keys starting with "__" and remove the prefix
+extracted_secrets=$(echo "$secrets" | jq 'with_entries(select(.key | startswith("__"))) | with_entries(.key |= ltrimstr("__"))')
+
+# Output the extracted secrets as JSON
+echo "$extracted_secrets"
