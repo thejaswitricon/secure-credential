5cddcdc9a39bf4a9b55f8dd5283141b88941b2c6
thejaswitricon <thejaswi.cy@triconinfotech.com>
2023-05-31 12:34:25 +0530

    fix:test

--- read_env.sh
+++ read_env.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
-# Read the secrets from the JSON file
-secrets=$(cat secrets.json)
+# Filter secrets based on keys starting with "__"
+secret_names=()
+while IFS='=' read -r key _; do
+  if [[ $key == __* ]]; then
+    secret_names+=("${key}")
+  fi
+done < <(env)
 
-# Extract the secret keys starting with "__" and remove the prefix
-extracted_secrets=$(echo "$secrets" | jq 'with_entries(select(.key | startswith("__"))) | with_entries(.key |= ltrimstr("__"))')
+# Retrieve secrets based on the filtered names
+for secret_name in "${secret_names[@]}"; do
+  secret_value="${!secret_name}"
+  echo "Secret $secret_name: $secret_value"
+done
 
-# Output the extracted secrets as JSON
-echo "$extracted_secrets"
+
+
+# #!/bin/bash
+
+# # Read the secrets from the JSON file
+# secrets=$(cat secrets.json)
+
+# # Extract the secret keys starting with "__" and remove the prefix
+# extracted_secrets=$(echo "$secrets" | jq 'with_entries(select(.key | startswith("__"))) | with_entries(.key |= ltrimstr("__"))')
+
+# # Output the extracted secrets as JSON
+# echo "$extracted_secrets"
